7c31d79592f56c56d5047b25e79c2836a59b6d5f
shivammathur4 <shivamm.4aug@gmail.com>
2021-04-29 10:53:42 +0530

    Added UC3

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -10,11 +10,35 @@ namespace SnakeLadder
     {
         static void Main(string[] args)
         {
-            const int playerOne = 1; 
+
+
+            const int noPlayOption = 0;
+            const int ladderOption = 1;
+            const int snakeOption = 2;
+            const int playerOne = 1;
+
             int positionOne = 0;
             Random random = new Random();
-            int dieRoll = random.Next(1, 7);  
+            int dieRoll = random.Next(1, 7);
             Console.WriteLine("Die Rolls: " + dieRoll);
+            int options = random.Next(0, 3);
+
+            switch (options)
+            {
+                case noPlayOption:
+                    positionOne = positionOne + 0;
+                    break;
+                case ladderOption:
+                    positionOne = positionOne + dieRoll;
+                    break;
+                case snakeOption:
+                    positionOne = positionOne - dieRoll;
+                    if (positionOne < 0)
+                    {
+                        positionOne = 0;
+                    }
+                    break;
+            }
         }
     }
 }
