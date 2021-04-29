ec725eab20ef0f3608039f7f590bff23060b8f6a
shivammathur4 <shivamm.4aug@gmail.com>
2021-04-29 10:59:14 +0530

    Added UC6

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -11,27 +11,32 @@ namespace SnakeLadder
         static void Main(string[] args)
         {
 
-
             const int noPlayOption = 0;
             const int ladderOption = 1;
             const int snakeOption = 2;
             const int playerOne = 1;
-            //variable
+            
             int positionOne = 0;
+            int diceRollCount = 0;
+            
             while (positionOne < 100)
             {
                 Random random = new Random();
                 int dieRoll = random.Next(1, 7);
+                diceRollCount++;
                 int options = random.Next(0, 3);
+                Console.WriteLine("Position: " + positionOne);
                 switch (options)
                 {
                     case noPlayOption:
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                     case ladderOption:
                         if (positionOne + dieRoll <= 100)
                         {
                             positionOne = positionOne + dieRoll;
                         }
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                     case snakeOption:
                         positionOne = positionOne - dieRoll;
@@ -39,9 +44,11 @@ namespace SnakeLadder
                         {
                             positionOne = 0;
                         }
+                        Console.WriteLine("Position: " + positionOne);
                         break;
                 }
             }
-            Console.WriteLine("Player Wins with Position :" + positionOne);
+            Console.WriteLine("Player Wins with Die Roll :" + diceRollCount);
         }
     }
+}
